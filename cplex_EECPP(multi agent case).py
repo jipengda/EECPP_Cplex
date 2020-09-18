7881636048f19ef867bdaae07535c09869bc949a
Jipeng Dai <jipengda@buffalo.edu>
2020-09-18 11:42:07 -0400

    Update cplex_EECPP(multi agent case).py

--- cplex_EECPP(multi agent case).py
+++ cplex_EECPP(multi agent case).py
@@ -11,20 +11,101 @@ import math
 import matplotlib.pyplot as plt
 
 #------------------------------------------------------------------------------
+#supplementary functions
+#------------------------------------------------------------------------------
+def check_obstacle(obstacles, lastNode, newNode, colNumber, rowNumber):
+    sideLength = 1
+    flag=1 # initialize flag=1
+    for obstacle in obstacles:
+        x3 = obstacle % colNumber
+        y3 = obstacle // colNumber
+        r_xmin = x3 - sideLength/2
+        r_xmax = x3 + sideLength/2
+        r_ymin = y3 - sideLength/2
+        r_ymax = y3 + sideLength/2
+        if RSIntersection(r_xmin, r_xmax, r_ymin, r_ymax, lastNode, newNode, colNumber, rowNumber) == 1:
+            flag = 0 #flag is variable to replace directly return False of True because we need to test all obstacles
+        else:
+            flag = 1
+        if flag == 0:#once flag change to 0, means one obstacle is hit, thus new node can not be added
+            return False
+    return True# This means all obstacles are not hit, new node can be considered to be added
+"""
+ * if [x1, x2] and [x3, x4] (x4 maybe smaller than x3) has interrection or has not，if yes: return 1， if no: return 0
+"""
+
+
+def IntervalOverlap(x1, x2, x3, x4):
+    t = 0    
+    if x3 > x4:
+       t = x3
+       x3 = x4	   
+       x4 = t
+    if x3 >= x2 or x4 <= x1:
+        return 0
+    else:
+        return 1
+    
+"""
+ * judge rectangular r and line segment AB has intersection or not，if yes: return 1，if no: return 0
+"""
+def RSIntersection(r_xmin,r_xmax,r_ymin, r_ymax, nodeA, nodeB, colNumber, rowNumber):
+    A_x = nodeA % colNumber
+    A_y = nodeA // colNumber
+    B_x = nodeB % colNumber
+    B_y = nodeB // colNumber
+    if (A_y == B_y):# line segement is parallel to x axis//线段平行于x轴
+        if A_y <= r_ymax and A_y >= r_ymin:
+            return IntervalOverlap(r_xmin, r_xmax, A_x,B_x)
+        else:
+            return 0
+
+	# Echange point A and point B to let point B has bigger y coordinate//AB两点交换，让B点的y坐标最大
+
+    # Exchange node A and node B, let B's y value is bigger
+    t = 0
+    if A_y > B_y:
+       t = A_y
+       A_y = B_y
+       B_y = t
+       t= A_x
+       A_x = B_x
+       B_x=t
+	
+    # In line segment//xianduan AB, to find point C and D
+    # Two points secure a line: (x-x1)/(x2-x1)=(y-y1)/(y2-y1)
+    k = (B_x - A_x)/(B_y - A_y)
+    if A_y < r_ymin:
+       D_y = r_ymin
+       D_x = k*(D_y - A_y) + A_x
+    else:
+       D_y=A_y
+       D_x=A_x
+    if B_y > r_ymax:
+       C_y = r_ymax
+       C_x = k*(C_y-A_y) + A_x
+    else:
+       C_y = B_y
+       C_x = B_x
+    if C_y >= D_y: # y axis has overlap
+       return IntervalOverlap(r_xmin, r_xmax,D_x, C_x)
+    else:
+       return 0
+
+#------------------------------------------------------------------------------
 #Data
 #------------------------------------------------------------------------------
 
 #add obstalces part
 agentNumber = 10
-colNumber = 3
-rowNumber = 3
+colNumber = 4
+rowNumber = 4
 Battery_capacity_constraint = 11
 nodesNumber = colNumber * rowNumber
 departurePoint = 0
 distance_lambda = 0.1164
 turn_gamma = 0.0173
-turn_gamma = 0.015
-obstacles=[]
+obstacles=[12,13]
 Nodes=[i for i in range(nodesNumber) if i not in obstacles and i!= departurePoint]
 NodesAndDeparturePoint = Nodes + [departurePoint]
 AllNodes = NodesAndDeparturePoint + obstacles
@@ -49,6 +130,13 @@ c = {(i,j):0 for i,j in edges}
 for i,j in edges:
     distance_cost = D[i][j] * distance_lambda
     c[(i,j)] = distance_cost
+    
+for o,p in edges:
+    View = check_obstacle(obstacles, o, p, colNumber, rowNumber)
+    if View == 0:
+        c[(o,p)] = math.inf
+    else:
+        pass
 
 
 q={(i,j,k):0 for i,j,k in arcs}
@@ -100,7 +188,7 @@ for a in agents:
         if j!=departurePoint:
             model.add_indicator( x[(a,i,j)],d[a,i] + 1 == d[a,j],name=None)
 
-model.parameters.timelimit=150
+model.parameters.timelimit=1800
 #model.parameters.timelimit=3600
 
 solution = model.solve(log_output=True)
